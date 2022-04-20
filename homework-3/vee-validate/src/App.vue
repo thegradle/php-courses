6f6dc2734ed30f3352406c8ac346b4d7b8d351c6
Ivan Adamchuk <adamchuk.v.1@gmail.com>
2022-04-20 12:22:24 +0300

    Done homework-3

--- homework-3/vee-validate/src/App.vue
+++ homework-3/vee-validate/src/App.vue
@@ -1,13 +1,16 @@
 <template>
-  <Form>
+  <Form :style="bg">
     <h1>Реєстрація</h1>
     <Field name="name" :rules="isRequired" placeholder="Ім'я"/>
     <ErrorMessage name="name" />
     <Field name="surname" :rules="isRequired" placeholder="Прізвище"/>
     <ErrorMessage name="surname" />
+    <input type="radio" name="sex" id="male" value="male" v-model="sex"><label for="male">Чоловік</label>
+    <input type="radio" name="sex" id="female" value="female" v-model="sex"><label for="female">Жінка</label>
+    <input type="radio" name="sex" id="other" value="other" v-model="sex" checked><label for="other">Інше</label>
     <Field name="tel" :rules="validateTel" placeholder="Телефонний номер"/>
     <ErrorMessage name="tel" />
-    <Field name="login" :rules="isRequired" placeholder="Логін"/>
+    <Field name="login" :rules="validateLogin" placeholder="Логін"/>
     <ErrorMessage name="login" />
   </Form>
 </template>
@@ -18,6 +21,12 @@ export default {
     Field,
     Form,
     ErrorMessage,
+  },
+  data() {
+    return {
+      bg: '',
+      sex: ''
+    }
   },
   methods: {
     isRequired(value) {
@@ -34,12 +43,32 @@ export default {
         }
         return 'Телефон введено не вірно!'
       }
+    },
+
+    validateLogin(value) {
+      if (this.isRequired(value)) {
+        if (/[A-Z]/.test(value) && /[_]/.test(value) && /\d\d\d$/.test(value)) {
+          return true;
+        }
+        return 'Логін введено не вірно! Він має містити одну велику літеру, один символ підкреслення та 3 цифри наприкінці!'
+      }
     }
   },
+  watch: {
+    sex(value) {
+      if (value == 'male') {
+        this.bg = 'background: blue; color: white;';
+      } else if (value == 'female') {
+        this.bg = 'background: pink';
+      } else {
+        this.bg = 'background: white';
+      }
+    }
+  }
 };
 </script>
 <style>
-  input, span {
+  input:not(input[type="radio"]), span {
     display: block;
     margin-bottom: 10px;
   }
